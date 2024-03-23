a0ee54a1df1d13d5a1e6d61af5acc40e4e4e9053
jorelwall154 <jorelwall154@gmail.com>
2024-03-23 11:23:35 -0300

    Add repository User Repository

---
+++ src/main/java/com/jorel/habits/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.jorel.habits.repositories;
+
+import com.jorel.habits.entities.User.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> searchUserByName();
+}
