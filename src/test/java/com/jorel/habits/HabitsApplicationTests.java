19c7b3424f669e58f293db5af8cbef45862c22e4
jorelwall154 <jorelwall154@gmail.com>
2024-03-23 11:10:51 -0300

    Initial commit

---
+++ src/test/java/com/jorel/habits/HabitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorel.habits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
